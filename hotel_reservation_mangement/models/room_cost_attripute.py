41fa8bb625a4980a730d31224f4a068311217134
emadraafatgad <emad.raafat155@gmail.com>
2022-02-25 18:21:34 -0800

    hotel total conditions

--- hotel_reservation_mangement/models/room_cost_attripute.py
+++ hotel_reservation_mangement/models/room_cost_attripute.py
@@ -45,13 +45,13 @@ class RoomCostAttribute(models.Model):
                     [('id','!=',rec.id),('hotel_id', '=', rec.hotel_id.id), ('room_type_id', '=', rec.room_type_id.id),
                      ('rate_supplier', '=', rec.rate_supplier.id),
                      ('meal_plan', '=', rec.meal_plan),
+                     '|', '&', ('date_from', '>=', rec.date_from), ('date_to', '<=', rec.date_to),
                      '|', '&', ('date_from', '<=', rec.date_from), ('date_to', '>=', rec.date_from),
                      '&', ('date_from', '<=', rec.date_to), ('date_to', '>=', rec.date_to), ])
                 if all_room_cost:
                     raise ValidationError("Dates overlapped, please make sure dates are correct " + str(all_room_cost.id) )
-                for room in all_room_cost:
-                    if rec.date_from > rec.date_to:
-                        raise ValidationError(_('date from must be less than date to'))
+                if len(rec.price_ids) > 0:
+                    raise ValidationError('Please one add price Lines')
 
     @api.onchange('period_lead_id')
     def get_period_lead_dates(self):

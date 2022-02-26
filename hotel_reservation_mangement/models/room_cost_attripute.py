3e620b6bba2115ac56d40fd8e41abca72a59403f
emadraafatgad <emad.raafat155@gmail.com>
2022-02-25 16:42:59 -0800

    hotel total rooms

--- hotel_reservation_mangement/models/room_cost_attripute.py
+++ hotel_reservation_mangement/models/room_cost_attripute.py
@@ -1,29 +1,41 @@
-from odoo import api , fields ,models
+from odoo import models, fields, api, _
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from odoo.exceptions import ValidationError, UserError
 
 
 class RoomCostAttribute(models.Model):
     _name = 'room.cost.attribute'
     _rec_name = 'room_type_id'
 
-    hotel_id = fields.Many2one('hotel.management',)
-    room_type_id = fields.Many2one('hotel.room.type',domain="[('hotel_id','=',hotel_id)]")
-    ordered_by = fields.Many2one('res.partner',string='Partner')
-    rate_supplier = fields.Many2one('res.partner')
-    date_from = fields.Date()
-    period_lead_id = fields.Many2one('date.range',string='Period')
-    date_to = fields.Date()
+    hotel_id = fields.Many2one('hotel.management', required=True)
+    room_type_id = fields.Many2one('hotel.room.type', required=True, domain="[('hotel_id','=',hotel_id)]")
+    ordered_by = fields.Many2one('res.partner', required=True, string='Partner')
+    rate_supplier = fields.Many2one('res.partner', required=True)
+    date_from = fields.Date( required=True)
+    period_lead_id = fields.Many2one('date.range', string='Period', )
+    date_to = fields.Date( required=True)
     note = fields.Char()
-    price_ids = fields.One2many('cost.per.person','room_type_id')
+    price_ids = fields.One2many('cost.per.person', 'room_type_id', required=True)
     # remove this only for db
-    cost = fields.Integer()
+    cost = fields.Integer(required=True)
 
     meal_plan = fields.Selection([
-        ('Soft Al Inclusive','Soft Al Inclusive'),
-        ('Al Inclusive','Al Inclusive'),
-        ('Half Board (HB)','Half Board (HB)'),
+        ('Soft Al Inclusive', 'Soft Al Inclusive'),
+
+        ('Half Board (HB)', 'Half Board (HB)'),
         ('Full Board (FB)', 'Full Board (FB)'),
-        ('Board (B)', 'Board (B)'),
-    ])
+
+    ], required=True)
+
+    # ('Al Inclusive', 'Al Inclusive'),
+    # ('Board (B)', 'Board (B)'),
+    @api.constrains('date_to', 'date_from')
+    def condition_on_date(self):
+        for rec in self:
+            if rec.date_from and rec.date_to:
+                if rec.date_from > rec.date_to:
+                    raise ValidationError(_('date from must be less than date to'))
 
     @api.onchange('period_lead_id')
     def get_period_lead_dates(self):
@@ -38,9 +50,7 @@ class CostPerPerson(models.Model):
     person_per_room = fields.Selection([
         ('1', 'Single'), ('2', 'Double'),
         ('3', 'Triple'), ('4', '4 Persons'),
-        ('5', '5 Persons')],string="Occupancy", default='1')
+        ('5', '5 Persons')], string="Occupancy", default='1')
     hotel_cost = fields.Integer("Room Cost")
     guest_price = fields.Integer("Selling Price")
     room_type_id = fields.Many2one('room.cost.attribute')
-
-
